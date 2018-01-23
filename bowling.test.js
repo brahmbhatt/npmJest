5b29ecd621643fb623cbb8b283140b54965b3573
brahmbhatt <margibrahmbhatt13@gmail.com>
2018-01-23 11:22:35 +0530

    test cse for all striks

--- bowling.test.js
+++ bowling.test.js
@@ -1,6 +1,19 @@
 let bowlingScore = require('./bowling');
-arr = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
-// describe('bowling score', () => {
-test('score is ', () => {
-  expect(bowlingScore(arr)).toBe(90);
+let arr = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
+let arr1 = [10,10,10,10,10,10,10,10,10,10,2,3];
+let arr2 = [5, 5, 3, 7, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+describe('bowling score', () => {
+  test('score is ', () => {
+    expect(bowlingScore(arr)).toBe(90);
+  });
+
+  test('for no strik or spare', () => {
+    expect(bowlingScore(arr1)).toBe(282);
+  });
+
+  test('for all striks is ', () => {
+    expect(bowlingScore(arr1)).toBe(282);
+  });
+
+
 });
