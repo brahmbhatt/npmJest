00f9596d82cd1a57a7c643093c3f95b72e0e4c47
brahmbhatt <margibrahmbhatt13@gmail.com>
2018-01-23 11:35:58 +0530

    testcase for three striks back to back

--- bowling.test.js
+++ bowling.test.js
@@ -4,6 +4,7 @@ let arr1 = [10,10,10,10,10,10,10,10,10,10,2,3];
 let arr2 = [5, 5, 3, 7, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 let arr3 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 7];
 let arr4 = [10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let arr5 = [10, 10, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 describe('bowling score', () => {
   test('score is ', () => {
     expect(bowlingScore(arr)).toBe(90);
@@ -27,5 +28,9 @@ describe('bowling score', () => {
 
   test('only one strike and all zero:', () => {
     expect(bowlingScore(arr4)).toBe(10);
+  });
+
+  test('three striks back to back', () => {
+    expect(bowlingScore(arr5)).toBe(81);
   });
 });
